4f82277058a59d7896914f4a157d253899b4d443
Ghost1499 <zgstv@yandex.ru>
2025-06-17 00:34:55 +0300

    make no distributed train.bat

--- train.bat
+++ train.bat
@@ -1,5 +1,19 @@
-python -m torch.distributed.launch --nproc_per_node=2 --master_port=29500 --use_env train.py ^
-    --gpu_id "0,1" ^
+@REM python -m torch.distributed.launch --nproc_per_node=2 --master_piport=29500 --use_env train.py ^
+@REM     --gpu_id "0,1" ^
+@REM     --data_path .\data ^
+@REM     --save_path .\result ^
+@REM     --model_name YONA ^
+@REM     --lr 5e-4 ^
+@REM     --backbone resnet50 ^
+@REM     --epoch 50 ^
+@REM     --train_clips 2 ^
+@REM     --test_clips 2 ^
+@REM     --scheduler step ^
+@REM     --n_threads 6 ^
+@REM     --batch_size 8
+
+python train.py ^
+    --gpu_id "0" ^
     --data_path .\data ^
     --save_path .\result ^
     --model_name YONA ^
@@ -10,4 +24,4 @@ python -m torch.distributed.launch --nproc_per_node=2 --master_port=29500 --use_
     --test_clips 2 ^
     --scheduler step ^
     --n_threads 6 ^
-    --batch_size 8
+    --batch_size 8 
